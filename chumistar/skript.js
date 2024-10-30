b4cf96442bc6c60f35e79202947366a854e9d21a
tyrimbet abylaikhan <abeke.618@gmail.com>
2024-10-30 18:46:29 +0500

    done

---
+++ chumistar/skript.js
@@ -0,0 +1,33 @@
+// var name = " a;soief"; //string
+// var age = 17; //number
+
+// var isNight =true;
+
+// var students = ['sd', 'du', 'sdf']
+
+// console.log(typeof name);
+// console.log(typeof name)
+// console.log(typeof name)
+//  var na = "yera";
+
+//  var me = "syl"
+
+//  console.log(na + me);
+
+
+//  var na = "5";
+
+//  var me = "9"
+
+//  console.log(na * me);
+
+ var name = "Abulaikhan";
+ var surname = "Turimbet";
+ var birthday = "01.90.2011";
+ var otan = "Kazahstan";
+
+ console.log("Menin atym" , name)
+ console.log("Menin surnam" , surname)
+ console.log("Menin tugankunim" , birthday)
+ console.log("Menin otanym" , otan)
+ 
